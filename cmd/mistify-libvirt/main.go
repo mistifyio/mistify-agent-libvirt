1b9b3d5d865be7e497da83ece6e714dc45864b84
Reha Sterbin <reha@omniti.com>
2015-02-20 19:30:27 +0000

    MIST-359 Forgot to switch the include

--- cmd/mistify-libvirt/main.go
+++ cmd/mistify-libvirt/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"os"
 
+	log "github.com/Sirupsen/logrus"
 	flag "github.com/docker/docker/pkg/mflag"
 	"github.com/mistifyio/mistify-agent-libvirt"
-	"github.com/mistifyio/mistify-agent/log"
 	"github.com/mistifyio/mistify-agent/rpc"
 )
 
